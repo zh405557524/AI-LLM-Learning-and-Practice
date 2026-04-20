9d56142bd6f2471cfbb2c5f1763ca735bde51e2f
soul <405557524@qq.com>
2026-04-20 17:41:07 +0800

    feat:python 第一课作业

--- 1.python/pythonCode/pythonProject/01_Python入门/作业/01作业.py
+++ 1.python/pythonCode/pythonProject/01_Python入门/作业/01作业.py
@@ -3,6 +3,9 @@
 1. 写一个初级程序：先输入自己的名字，如输入：张三，则打印"大家好，我是 张三"
 '''
 
+# name = input('Enter your name: ')
+
+# print("大家好，我是",name)
 
 
 '''
@@ -10,7 +13,10 @@
 在控制台分别输入3个变量a,b,c，然后用print输出这3个变量组成的字符串，中间要求使用分隔符加号（+）连接, 要求使用sep
 例如：输入3个字符串“Python”、“is”、“Wonderful”，结果显示为“Python+is+Wonderful”
 '''
-
+# a = input()
+# b = input()
+# c = input()
+# print(a+" +",b+" +",c)
 
 
 '''
@@ -18,3 +24,7 @@
    公式： 面积S = 半径r * 半径r * 3.14
 		 周长C = 半径r * 3.14 * 2
 '''
+r =int(input("请输入半径:"))
+
+print("面试S=",r*r*3.14)
+print("周长C=",r*3.14*2)
